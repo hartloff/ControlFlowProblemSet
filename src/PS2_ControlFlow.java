1bcd6eec8cf997044f883dc1c626f0a0f221d28f
jessehartloff <hartloff@buffalo.edu>
2017-02-05 15:10:44 -0500

    initial commit

---
+++ src/PS2_ControlFlow.java
@@ -0,0 +1,106 @@
+public class PS2_ControlFlow {
+
+
+    static void Q1(){
+        // print all integers from 1 to 30 (including 1 and 30)
+
+    }
+
+
+    static void Q2(){
+        // print all integers from 0 to 29 (including 0 and 29)
+
+    }
+
+
+    static void Q3(){
+        // print all integers from 200 to 230 (including 200 and 230)
+
+    }
+
+
+    static void Q4(){
+        // print the numbers 40 through 50, then the numbers 70 through 80 (including 40, 50, 70, and 80)
+
+    }
+
+
+    static void Q5(){
+        // print the numbers 100 through 120, then the numbers 60 through 70 (including 100, 120, 60, and 70)
+
+    }
+
+
+    static void Q6(){
+        // print the decreasing integers from 35 to 0 (including 35 and 0)
+
+    }
+
+
+    static void Q7(){
+        // print all even integers between 1 and 50 (including 50)
+
+    }
+
+
+    static void Q8(){
+        // print all odd integers between 1 and 50 (including 1)
+
+    }
+
+
+    static void Q9(){
+        // print all negative integers between -25 and 100 (including -25)
+
+    }
+
+
+    static void Q10(){
+        // print all multiples of 10 between 1 and 200 (including 200)
+        // in other words, count by tens up to 200
+
+    }
+
+
+    static void Q11(){
+        // print each integer between 50 and 53 5 times each (including 50 and 53)
+
+    }
+
+
+    static void Q12(){
+        // print the integers from 1 to 20 (including 1 and 20) except if the number is even print "even" instead
+        // of the number
+
+    }
+
+
+    static void Q13(){
+        // print the integers from 1 to 20 (including 1 and 20) except if the number is even print "even" instead
+        // of the number and if the number is odd print "odd" instead of the number
+
+    }
+
+
+    static void Q14(){
+        // print the integers from -10 to 25 (including -10 and 25) except if the number is negative print "<0"
+        // instead of the number, if the number is positive print ">0" instead of the number, and if the number is 0
+        // print "=0" instead of 0.
+
+    }
+
+
+    static void Q15(){
+        // print the numbers from 1 to 40 (including 1 and 40) except if the number is even print "even" instead
+        // of the number and if the number is greater than 25 print ">25" instead of the number. For numbers that
+        // are both even and greater than 25 print "even>25"
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
